d906087d945d21653dc4f3861766bccff75f93ea
Jonathan <jonathaan.olsson@gmail.com>
2018-10-10 09:12:16 +0200

    Recorder and shizzle

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
 from wit import Wit
+import requests
+import json
+from Recorder import record_audio, read_audio
 
-access_token = "EVBUV3HCYZYNAQFNVUMNSSSETBACJJCL"
+API_ENDPOINT = 'https://api.wit.ai/speech'
 
+access_token = 'EVBUV3HCYZYNAQFNVUMNSSSETBACJJCL'
 client = Wit(access_token)
-rep = client.message('Jag vill ha en hamburgare')
 
-print(rep)
+def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
+    
+    # record audio of specified length in specified audio file
+    record_audio(num_seconds, AUDIO_FILENAME)
+    
+    # reading audio
+    audio = read_audio(AUDIO_FILENAME)
+    
+    # defining headers for HTTP request
+    headers = {'authorization': 'Bearer ' + access_token,
+               'Content-Type': 'audio/wav'}
 
-# resp = None
-#with open('test.wav', 'rb') as f:
-#  resp = client.speech(f, None, {'Content-Type': 'audio/wav'})
-#print('Yay, got Wit.ai response: ' + str(resp))
+    # making an HTTP post request
+    resp = requests.post(API_ENDPOINT, headers = headers,
+                         data = audio)
+    
+    # converting response content to JSON format
+    data = json.loads(resp.content)
+    
+    # get text from data
+    text = data['_text']
+    
+    # return the text
+    return text
 
 
+if __name__ == "__main__":
+    text =  RecognizeSpeech('myspeech.wav', 4)
+    client.message(text)
+    print("\nYou said: {}".format(text))
 
