a5876952a91e9ea72e67ac2b8b25350cc3a3c63a
DESKTOP-3851F6T\johan <johansson.marcus94@gmail.com>
2018-10-11 11:51:27 +0200

    Get entities and values

--- app.py
+++ app.py
@@ -8,6 +8,25 @@ API_ENDPOINT = 'https://api.wit.ai/speech'
 access_token = 'EVBUV3HCYZYNAQFNVUMNSSSETBACJJCL'
 client = Wit(access_token)
 
+def decoder (resp):
+    #keys  = resp.keys()
+    #items = resp.items()
+    #values = resp.values()
+
+    #print(keys)
+    #print(items)
+    #print(values)
+
+    for key, value in resp.items():
+        if(key == 'entities'):
+            print(key)
+            print("\n")
+            for k in value.keys():
+                print("Entity: " + k)
+                p = value[k];
+                print(p[0]['value'])
+            
+
 def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
     
     # record audio of specified length in specified audio file
@@ -23,9 +42,13 @@ def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
     # making an HTTP post request
     resp = requests.post(API_ENDPOINT, headers = headers,
                          data = audio)
-    
+
+
+
     # converting response content to JSON format
     data = json.loads(resp.content)
+
+    decoder(data)
     
     # get text from data
     text = data['_text']
@@ -34,8 +57,12 @@ def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
     return text
 
 
+
+
 if __name__ == "__main__":
-    text =  RecognizeSpeech('myspeech.wav', 4)
-    client.message(text)
+    ##text = RecognizeSpeech('myspeech.wav', 4)
+    text = 'En hamburgare tack'
+    resp = client.message(text)
+    decoder(resp)
     print("\nYou said: {}".format(text))
 
