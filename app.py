2f0fbfb11b4311388a81226854b2c46127d39a93
DESKTOP-3851F6T\johan <johansson.marcus94@gmail.com>
2018-10-12 10:19:48 +0200

    Added budget voice answer

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from wit import Wit
 import requests
 import json
+import pyttsx3
 from Recorder import record_audio, read_audio
+
+
+
 
 API_ENDPOINT = 'https://api.wit.ai/speech'
 
@@ -9,14 +13,8 @@ access_token = 'EVBUV3HCYZYNAQFNVUMNSSSETBACJJCL'
 client = Wit(access_token)
 
 def decoder (resp):
-    #keys  = resp.keys()
-    #items = resp.items()
-    #values = resp.values()
-
-    #print(keys)
-    #print(items)
-    #print(values)
-
+    print(resp)
+    entities = []
     for key, value in resp.items():
         if(key == 'entities'):
             print(key)
@@ -25,7 +23,29 @@ def decoder (resp):
                 print("Entity: " + k)
                 p = value[k];
                 print(p[0]['value'])
-            
+                tuple1 = (k , p[0]['value'])
+                entities.append(tuple1)
+    return entities
+
+
+def makeAnswer(entities):
+    text ='You ordered '
+
+    for ent in entities:
+        value = ent[1]
+        print(ent[0])
+        if ent[0] == 'food_type':
+            text+= value + ' '
+        elif ent[0] == 'number':
+            text += str(value) + ' '
+        elif ent[0] == 'drink':
+            text+= value
+
+
+    print(text)
+    return text
+
+
 
 def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
     
@@ -61,8 +81,9 @@ def RecognizeSpeech(AUDIO_FILENAME, num_seconds = 5):
 
 if __name__ == "__main__":
     ##text = RecognizeSpeech('myspeech.wav', 4)
-    text = 'En hamburgare tack'
+    engine = pyttsx3.init()
+    text = '3 hamburgare och 3 hamburgare tack'
     resp = client.message(text)
-    decoder(resp)
-    print("\nYou said: {}".format(text))
+    engine.say(makeAnswer(decoder(resp)))
+    engine.runAndWait()
 
